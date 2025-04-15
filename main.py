66720be3f1e18c8a3b7a73eb31d686b53bf89e13
OllyOzzy <oliver.ozga.861@my.aquinas.me>
2025-04-15 14:49:32 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#main
+#opens and reads the file
+fin = open("image. ppm")
+image = fin.read()
+#print(self.image)
+
+class image_editing:
+
+    def __init__(self):
+        pass
+    
+    def obtaining_key_values(self, image_text):
+        image_text_list = []
+        for num in image_text.split():
+            image_text_list.append(num)
+        self.magic_number = image_text_list[0]
+        self.width = int(image_text_list[1])
+        self.length = int(image_text_list[2])
+        self.max_colour_val = image_text_list[3]
+        self.each_pixel_image_text = image_text_list[4:-1]
+        return self.each_pixel_image_text
+
+    def hold_pix(self):
+        grid = []
+        while len(grid) < self.length:
+            row_list = []
+            while len(row_list) < self.width:
+                row_list.append(self.each_pixel_image_text[0:3])
+                self.each_pixel_image_text = self.each_pixel_image_text[3:]
+            grid.append(row_list)
+        return grid
+
+bot = image_editing()
+bot.obtaining_key_values(image)
+print(bot.hold_pix())
